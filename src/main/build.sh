54f394dda669512923b3c23ab202151e0d6b7242
刘祥超 <iwind.liu@gmail.com>
2019-06-19 09:17:05 +0800

    修改编译脚本、服务脚本

--- src/main/build.sh
+++ src/main/build.sh
@@ -2,8 +2,9 @@
 
 . utils.sh
 
-# darwin
 export GOPATH=`pwd`/../../
+
+# darwin
 export GOOS=darwin
 export GOARCH=amd64
 
@@ -11,7 +12,6 @@ build
 
 
 # linux 64
-export GOPATH=`pwd`/../../
 export GOOS=linux
 export GOARCH=amd64
 
@@ -19,35 +19,30 @@ build
 
 
 # linux 32
-export GOPATH=`pwd`/../../
 export GOOS=linux
 export GOARCH=386
 
 build
 
 # linux arm64
-export GOPATH=`pwd`/../../
 export GOOS=linux
 export GOARCH=arm64
 
 build
 
 # linux arm32
-export GOPATH=`pwd`/../../
 export GOOS=linux
 export GOARCH=arm
 
 build
 
 # windows 64
-export GOPATH=`pwd`/../../
 export GOOS=windows
 export GOARCH=amd64
 
 build
 
 # windows 32
-export GOPATH=`pwd`/../../
 export GOOS=windows
 export GOARCH=386
 
